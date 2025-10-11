3c2292596cac09350645fd392e521f666edee3d7
Nicolas Dávila <nicolasdavilafsantos@gmail.com>
2025-10-11 16:07:06 -0300

    feat: criar landing page

--- index.php
+++ index.php
@@ -5,11 +5,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
+<header>
+
+</header>
+
 <body>
 
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-sm-8">
+                <h2 style="text-align: left;">Da prateleira ao caixa,</h2>
+                <h2 style="text-align: left;">tudo em um só lugar!</h2>
+                <p style="text-align: left;">
+                    Simplificando a gestão de produtos e finanças
+                    para lojas.
+                </p>
+                <button class="comecar_agora">Começar agora</button>
+            </div>
+            <div class="col-sm-4">
+                <img src="./assets/img/retrato-de-um-homem-bonito-sorridente-em-oculos-Photoroom.png" alt="">
+            </div>
+        </div>
+    </div>
+
 </body>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 
 </html>
