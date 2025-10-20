092859433a1e88c740ea7cb9cbde9ba67b267b0b
nicolas-davila <nicolasdavilafsantos@gmail.com>
2025-10-20 20:05:30 -0300

    feat: Criar landing page

--- index.php
+++ index.php
@@ -14,21 +14,29 @@
 <body>
 
     <!-- ===== Navbar ===== -->
-    <nav class="navbar navbar-expand-lg">
+    <nav class="navbar navbar-expand-lg mt-4" style="border-bottom: none; box-shadow: none;">
         <div class="container">
-            <a class="navbar-brand" href="#">
-                <img src="./assets/img/logo.png" alt="Logo">
+            <a class="navbar-brand d-flex align-items-center" href="#">
+                <img src="./assets/img/logo.png" alt="Logo" style="height:40px; margin-right:10px;">
                 StoreManager
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon" style="filter: invert(1);"></span>
             </button>
             <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item"><a class="nav-link active" href="#">Entrar</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Como Funciona</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Recursos</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Preços</a></li>
+                <ul class="nav nav-pills nav-fill">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Entrar</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Como Funciona</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Recursos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Preços</a>
+                    </li>
                 </ul>
             </div>
         </div>
