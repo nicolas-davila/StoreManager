bb5f02be7f70b8ab90bb398c6605c5209fee6d9b
nicolas-davila <nicolasdavilafsantos@gmail.com>
2025-10-20 20:40:24 -0300

    feat: Criar landing page

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
@@ -42,7 +43,9 @@
         </div>
     </nav>
 
-    <div class="d-flex justify-content-center align-items-center vh-100">
+    <div class="d-flex justify-content-center align-items-center" style="padding-top: 15rem;
+    animation: slideInFromRight 0.8s ease-out 0.5s forwards;
+    opacity: 0;">
         <div class="text-start">
             <h2>Da prateleira ao caixa,</h2>
             <h2>tudo em um só lugar!</h2>
@@ -50,11 +53,15 @@
                 Simplificando a gestão de produtos e finanças
                 para lojas.
             </p>
-            <div class="d-grid gap-2 col-6 mx-auto">
-                <button class="btn btn-primary comecar_agora" type="button">Button</button>
+            <div class="d-grid gap-2 col-6 mx-auto pt-2">
+                <button class="btn btn-primary comecar_agora" type="button">Vamos começar?</button>
             </div>
         </div>
 
+    </div>
+
+    <div class="d-flex justify-content-center mais_conteudo">
+        <i class="bi bi-arrow-down" style="font-size: 2rem; color: #fff;"></i>
     </div>
 
 </body>
